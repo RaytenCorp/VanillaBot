83c8a7fd19a3e8462757e7f858f2053770079dfd
TFMB <mail@vanilla-station.ru>
2025-01-13 22:49:04 +0300

    first step

---
+++ _Commands/EventReportComand.cs
@@ -0,0 +1,17 @@
+using Discord;
+using Discord.Interactions;
+using System;
+using System.Threading.Tasks;
+
+namespace VanillaBot
+{
+    public class EventReportCommand : InteractionModuleBase<SocketInteractionContext>
+    {
+        [SlashCommand("eventreport", "Составить отчёт о проведённом ивенте")]
+        public async Task SendEventReportAsync()
+        {
+            Console.WriteLine("команда поступила");
+        }
+
+    }
+}
