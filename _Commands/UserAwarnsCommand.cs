a28134460d7190471b338986c5375232ca0eab1d
TFMB <mail@vanilla-station.ru>
2025-03-31 16:04:38 +0300

    somelogs

--- _Commands/UserAwarnsCommand.cs
+++ _Commands/UserAwarnsCommand.cs
@@ -11,35 +11,39 @@ public class UserAwarnsCommand : InteractionModuleBase<SocketInteractionContext>
     [SlashCommand("userawarns", "Показать все аварны указанного пользователя и их даты сгорания.")]
     public async Task UserAwarnsAsync([Summary("пользователь", "Укажите пользователя для проверки варнов.")] IUser user)
     {
-        // Получаем все аварны пользователя
-        var userWarns = AWarnManager.GetAwarnsForUser(user.Id);
-
-        if (userWarns.Count == 0)
+        try
         {
-            await RespondAsync($"{user.Username} не имеет аварнов.", ephemeral: true);
-            return;
+            var userWarns = AWarnManager.GetAwarnsForUser(user.Id);
+
+            if (userWarns.Count == 0)
+            {
+                await RespondAsync($"{user.Username} не имеет аварнов.", ephemeral: true);
+                return;
+            }
+
+            var embed = new EmbedBuilder()
+                .WithTitle($"Аварны пользователя {user.Username}")
+                .WithColor(Color.Blue)
+                .WithTimestamp(DateTimeOffset.Now);
+
+            foreach (var warn in userWarns)
+            {
+                var expirationDate = warn.AwarnDate.AddDays(90);
+                string Typeofawarn = warn.Type == AwarnType.FullWarn ? "Аварн" : "Полуварн";
+                
+                embed.AddField(
+                    $"{Typeofawarn} от {warn.AwarnDate:dd.MM.yyyy}",
+                    $"**Причина:** {warn.Reason}\n" +
+                    $"**Действует до:** {expirationDate:dd.MM.yyyy}",
+                    true);
+            }
+
+            await RespondAsync(embed: embed.Build(), ephemeral: true);
         }
-
-        var embed = new EmbedBuilder()
-            .WithTitle($"Аварны пользователя {user.Username}")
-            .WithColor(Color.Blue)
-            .WithTimestamp(DateTimeOffset.Now);
-
-        // Добавляем информацию о каждом аварне
-        foreach (var warn in userWarns)
+        catch (Exception ex)
         {
-            var expirationDate = warn.AwarnDate.AddDays(90);
-
-           string Typeofawarn = warn.Type == AwarnType.FullWarn ? "Аварн" : "Полуварн";
-
-            embed.AddField($"Аварн от {warn.AwarnDate:dd MMM yyyy}",
-                $"Дата окончания: {expirationDate:dd MMM yyyy}",
-                false);
-            embed.AddField($"Причина: {warn.Reason}",
-                $"Тип: {Typeofawarn}",
-                false);
+            Console.WriteLine($"Ошибка в команде userawarns: {ex}");
+            await RespondAsync("Произошла ошибка при обработке команды", ephemeral: true);
         }
-
-        await RespondAsync(embed: embed.Build(), ephemeral: true);
     }
 }
